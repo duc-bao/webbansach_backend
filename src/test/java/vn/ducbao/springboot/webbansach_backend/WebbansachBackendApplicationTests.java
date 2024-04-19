96780ff4494a9538712247681d8bd41738a3781e
ducbao <truongducbao290402@gmail.com>
2024-04-19 19:42:30 +0700

    first commit

---
+++ src/test/java/vn/ducbao/springboot/webbansach_backend/WebbansachBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.ducbao.springboot.webbansach_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebbansachBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
